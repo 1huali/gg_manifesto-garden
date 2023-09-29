a6a389ffb1657738f1dc70cfd50903d3c48eeb42
1huali <wanhuali2000@gmail.com>
2023-09-29 14:38:57 -0400

    Setup plain files

---
+++ gg_manifesto-garden/js/script.js
@@ -0,0 +1,14 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+window.onload = (event) => {
+
+    
+}; //end windown on load
