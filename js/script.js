fef425448ca901c738ddc7245be3de3b4051bac3
1huali <wanhuali2000@gmail.com>
2023-09-29 14:51:39 -0400

    map implemented

--- js/script.js
+++ js/script.js
@@ -10,5 +10,26 @@ author, and this description to match your project!
 
 window.onload = (event) => {
 
+   //MAP SETTING ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀ 
+// Set the maximum and minimum zoom levels
+    let maxZoomLevel = 5;
+    let minZoomLevel = 2;
+    if (L.Browser.mobile) {
+        maxZoomLevel = 5;
+        minZoomLevel = 4;
+        }
+// Define the bounds of the map
+const southWest = L.latLng(-90, -180); // Bottom-left corner of the world
+const northEast = L.latLng(90, 180);   // Top-right corner of the world
+const bounds = L.latLngBounds(southWest, northEast);
+
+    // Initialize the map with minZoom and maxZoom options
+let map = L.map("map", {
+    minZoom: minZoomLevel,
+    maxZoom: maxZoomLevel,
+    maxBounds: bounds, // Restrict the map view to the specified bounds
+  // }).setView([0, -90], 5); //sabine's settings
+  }).setView([51.505, -0.09], 4); //old settings
     
+
 }; //end windown on load
