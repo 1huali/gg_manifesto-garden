04bd9039e307b2a6bf4b30030983c73c12c0fd3a
1huali <wanhuali2000@gmail.com>
2023-11-24 15:02:24 -0500

    frame set

--- js/script.js
+++ js/script.js
@@ -1,11 +1,46 @@
 /**
-Title of Project
-Author Name
+Jardin Manifeso Garden
+Wawa Li pour Galerie Galerie
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
 */
 
 "use strict";
+window.onload = (event) => {
 
-// Code goes here
+
+// PARAMÈTRE pour BOUTONS HTML ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀ 
+// manifesto button:
+document.getElementById("manifesto-button").addEventListener("click", function(){
+document.getElementById("manifesto-container").style.display="block"
+});
+document.getElementById("manifesto-container-close-button").addEventListener("click", function(){
+    document.getElementById("manifesto-container").style.display="none"
+});
+// intro button:
+    document.getElementById("intro-container-close-button").addEventListener("click", function(){
+        document.getElementById("intro-container").style.display="none"
+    });
+//theme button:
+let backgrounds=[
+    {"src": "assets/images/cascade-img.jpeg","color":"white","theme":"default"},
+    {"src": "assets/images/cascade2-img.jpeg","color":"fuchsia","theme":"cascade2"}  
+  ];
+  let currentBg=0;
+  document.getElementById("theme-button").addEventListener("click", changeBgPicture);
+function changeBgPicture(){
+    //changes theme array:
+    if (currentBg >= backgrounds.length-1) {
+        console.log(backgrounds[currentBg].src);
+        currentBg=0;
+      }
+      else {
+        console.log(backgrounds[currentBg].src);
+        currentBg= currentBg+1;
+        }
+        //implements new bg image
+        document.getElementById("bg-img").style.background= "url(`../"+[currentBg].src+"`);"; //??doesnt work
+        document.getElementById("body").style.color= backgrounds[currentBg].color;
+        document.getElementById("bg-theme").innerHTML=backgrounds[currentBg].theme;
+}
+
+}
