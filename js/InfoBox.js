93bebf6db63af427d4e91e28e6fcae066c8ae731
1huali <wanhuali2000@gmail.com>
2023-12-08 14:43:42 -0500

    seed box unique and draggable

--- js/InfoBox.js
+++ js/InfoBox.js
@@ -4,41 +4,111 @@ class InfoBox{
         this.description=description,
         this.axis=axis;
         this.linkNum=linkNum;
-        this.element=null;
+        this.boxIsDragging =false;
+
+// MAKE SEPERATE INFO BOX INSTANCES FOR EACH SEED:
+        //create element:
+        this.el = document.createElement("div");
+        this.el.classList.add("seed-container");
+        document.querySelector("#bg-img").appendChild(this.el);
+        this.el.style.display = "none";
+        //active draggable functionality:
+        this.setAsDraggableElement(this.el);
+
+        //create close button
+        this.buttonEl = document.createElement("input");
+        this.buttonEl.type = "button";
+        this.buttonEl.classList.add("close-buttons");
+        this.buttonEl.classList.add("buttons");
+        this.buttonEl.value = "X";
+        this.el.appendChild(this.buttonEl);
+
+        //creating seed content div : 
+        this.seedContent =  document.createElement("div");
+        this.el.appendChild(this.seedContent);
+        //creating seed title div : 
+        this.linkTitle =  document.createElement("div");
+        this.seedContent.appendChild(this.linkTitle)
+        this.linkTitle.innerHTML="✿ Link: "+this.link;
+        //implementing seed description in the div : 
+        this.linkDesc =  document.createElement("div");
+        this.seedContent.appendChild(this.linkDesc);
+        this.linkDesc.innerHTML="✿ Description: "+this.description;
+        //creating seed link div : 
+        this.linkAxis =  document.createElement("div");
+        this.seedContent.appendChild(this.linkAxis);
+        this.linkAxis.innerHTML = "✿ Axis: "+ this.axis;
+        //implementing seed link : 
+        this.linkImg=  document.createElement("div");
+        this.seedContent.appendChild(this.linkImg);
+        this.linkImg.innerHTML = "✿ Img:";
+
+
+        this.linkOpen =  document.createElement("div");
+        this.seedContent.appendChild(this.linkOpen);
+        this.linkOpen.innerHTML = "CLICK HERE TO ACCESS RESOURCE";
+
 
         let self=this;
 
         //close seedbox on button click:
-        document.getElementById("close-seedbox-button").addEventListener("click", function(){
-            document.getElementById("seedBoxDiv").style = "display: none;"
-        });
+        this.buttonEl.addEventListener("click", function(){
+            self.el.style= "display: none;"
+         });
     }
+
     openSeedbox(){
-        // $("#talkBoxDialog").dialog('open');
-        document.getElementById("seedBoxDiv").style = "display: block;"
 
-        // if (this.element===null) {
-        //     let div = document.createElement("div");
-        //                 div.id="seedboxEl"+this.linkNum;
-        //                 div.classList.add('seed-container');
-        //                 document.body.appendChild(div)
-        //                 this.element=div;
-        //                 // console.log(this.element)
-        //                 div.innerHTML="✿ Link: "+this.link;
-        //                 div.innerHTML="✿ Description: "+self.description;
-        //                 div.innerHTML="✿ Axis: "+this.axis;
-        //               }
+        let self = this;
+        
+        this.el.style.display ="block";
 
-        // this.element.style = "display: block";
-
-        document.getElementById("linkTitle").innerHTML="✿ Link: "+this.link;
-        document.getElementById("linkDescription").innerHTML="✿ Description: "+self.description;
-        document.getElementById("linkAxis").innerHTML="✿ Axis: "+this.axis;
-
-        document.getElementById("linkOpen").addEventListener("click", function(){
+       this.linkOpen.addEventListener("click", function(){
             console.log(`${self.link}`); //?? GIVES UNDEFINED
-            window.open(`${this.link}`);
+            window.open(`${self.link}`);
         })
     }
+
+    setAsDraggableElement(elmnt) {
+        let self = this;
+    //     let boxIsDragging = false;
+        elmnt.addEventListener("mousedown", dragMouseDown);
+      elmnt.addEventListener("mouseup", closeDragElement);
+      elmnt.addEventListener("mousemove", elementDrag);
+    
+        function dragMouseDown(event) {
+          // Add event as a parameter
+          console.log(event.target);
+          
+         if(!(event.target===document.querySelector("#close-button"))){
+          event.preventDefault();
+    
+          if (self.boxIsDragging === false ) {
+            console.log("here");
+            self.boxIsDragging = true;
+           
+          }
+        }
+        }
+    
+        function elementDrag(event) {
+          if (self.boxIsDragging === true) {
+            event.preventDefault();
+            // calculate the new cursor position:
+        ;
+            // set the element's new position:
+            elmnt.style.top = event.clientY - elmnt.getBoundingClientRect().height/2+ "px";
+            elmnt.style.left = event.clientX - elmnt.getBoundingClientRect().width/2 + "px";
+          }
+        }
+    
+    
+        function closeDragElement() {
+          if (self.boxIsDragging === true) {
+            console.log("up");
+            self.boxIsDragging = false;
+          }
+        }
+      } //end dragElement();
 }
 
